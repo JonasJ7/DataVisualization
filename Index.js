756c7c2df34fab1a534e01dc10e81cb8e4f50b5b
JonasJ7 <jonasjuhlert7@gmail.com>
2022-03-10 13:25:39 +0100

    drawing some circles

--- Index.js
+++ Index.js
@@ -1,14 +1,31 @@
-var slowspeed = 5;
-var fastspeed = 28;
+var w = 1000;
+var h = 1000;
+var rad = 50;
+var leftMargin = w/4;
+var dogdata = [10,5,30,20].sort(d3.ascending);
 
-var walker=50;
-var cyclist=100;
+var svg = d3.select("svg").attr("width", w).attr("height", h).style("background-color", "black");
 
-var svg = d3.select("svg")
-	.attr("width",1000)
-	.attr("height",800)
+var circle = d3.select("svg").selectAll("circle")
+    .data(dogdata)
+    .join("circle")
+    .attr("cx", function (d, i) {
+        console.log(d);
+        console.log(i);
+        return leftMargin+i * rad*2;
+    })
+    .attr("cy", h/2)
+    .attr("r", function (d) {
+        return d*rad/4;
+    })
+    .attr("fill", "none")
+    .attr("stroke", "white")
+    .style("stroke-width", "5");
 
 
+
+
+/*
 svg.append("line")
 .attr("x1",walker)
 .attr("x2", walker+slowspeed)
@@ -22,3 +39,4 @@ svg.append("line2")
 .attr("y1", cyclist)
 .attr("y2",cyclist)
 .attr("stroke", "red")
+*/
